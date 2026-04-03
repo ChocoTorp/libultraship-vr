5ab92438de42425f4f7563d59c958adcc8e3a585
John Schmitz <schmitty247@gmail.com>
2026-04-03 14:47:55 -0500

    Android: revert NavEnableKeyboard (triggers soft keyboard/window unfocus)

--- src/ship/window/gui/Gui.cpp
+++ src/ship/window/gui/Gui.cpp
@@ -142,18 +142,12 @@ bool Gui::GamepadNavigationEnabled() {
 
 void Gui::BlockGamepadNavigation() {
     mImGuiIo->ConfigFlags &= ~ImGuiConfigFlags_NavEnableGamepad;
-#if defined(__ANDROID__)
-    mImGuiIo->ConfigFlags &= ~ImGuiConfigFlags_NavEnableKeyboard;
-#endif
 }
 
 void Gui::UnblockGamepadNavigation() {
     if (Ship::Context::GetRawInstance()->GetConsoleVariables()->GetInteger(CVAR_IMGUI_CONTROLLER_NAV, 0) &&
         GetMenuOrMenubarVisible()) {
         mImGuiIo->ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-#if defined(__ANDROID__)
-        mImGuiIo->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-#endif
     }
 }
 
@@ -235,10 +229,8 @@ void Gui::DrawMenu() {
         if (Ship::Context::GetInstance()->GetConsoleVariables()->GetInteger(CVAR_IMGUI_CONTROLLER_NAV, 0) &&
             GetMenuOrMenubarVisible()) {
             mImGuiIo->ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-            mImGuiIo->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
         } else {
             mImGuiIo->ConfigFlags &= ~ImGuiConfigFlags_NavEnableGamepad;
-            mImGuiIo->ConfigFlags &= ~ImGuiConfigFlags_NavEnableKeyboard;
         }
     }
 #else
